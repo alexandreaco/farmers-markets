c4c7d2d664585efe77355410b24f8c04f9b7e90a
Alex Defreitas <alexandreadef@gmail.com>
2016-08-19 14:03:56 -0400

    Installs router

--- app/main.js
+++ app/main.js
@@ -4,14 +4,20 @@ import { render } from 'react-dom'
 import { Provider } from 'react-redux';
 import App from './components/App';
 import configureStore from './store/configureStore';
+import { Router, Route, browserHistory } from 'react-router'
+import { syncHistoryWithStore } from 'react-router-redux'
 
 const store = configureStore();
+const history = syncHistoryWithStore(browserHistory, store)
 
 var css = require("./scss/main.scss");
 
 render(
   <Provider store={store}>
-    <App />
+    <Router history={history}>
+      <Route path="/" component={App}>
+      </Route>
+    </Router>
   </Provider>,
   document.getElementById('app')
 )
