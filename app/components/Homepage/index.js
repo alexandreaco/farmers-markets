7ef5dde5bda0bf955adb33bc00dfb0ae13c8aad4
Alex Defreitas <alexandreadef@gmail.com>
2016-08-19 14:35:29 -0400

    Router really working this time

--- app/components/Homepage/index.js
+++ app/components/Homepage/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router'
 
 class App extends Component {
 
@@ -11,11 +10,7 @@ class App extends Component {
   render() {
     return (
       <div className="container">
-        <nav>
-          <Link to="/">Home</Link>
-        </nav>
-
-        {this.props.children}
+        <h1>Homepage</h1>
       </div>
     )
   }
--- app/main.js
+++ app/main.js
@@ -2,9 +2,8 @@ import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom'
 import { Provider } from 'react-redux';
-import App from './components/App';
 import configureStore from './store/configureStore';
-import { Router, Route, browserHistory } from 'react-router'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
 
 const store = configureStore();
@@ -12,10 +11,15 @@ const history = syncHistoryWithStore(browserHistory, store)
 
 var css = require("./scss/main.scss");
 
+import App from './components/App';
+import Homepage from './components/Homepage';
+import LocationDetail from './components/LocationDetail';
+
 render(
   <Provider store={store}>
     <Router history={history}>
       <Route path="/" component={App}>
+        <IndexRoute component={Homepage}/>
       </Route>
     </Router>
   </Provider>,
