f44149e0ebeca33a9e3e14e76a8d6d4c83f10f1c
Alexandrea Defreitas <alexandreadef@gmail.com>
2016-10-01 14:55:48 -0400

    Installs homepage route

--- app/main.js
+++ app/main.js
@@ -2,7 +2,6 @@ import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom'
 import { Provider } from 'react-redux';
-import App from './components/App';
 import configureStore from './store/configureStore';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
@@ -12,10 +11,14 @@ const history = syncHistoryWithStore(browserHistory, store)
 
 var css = require("./scss/main.scss");
 
+import App from './components/App';
+import Homepage from './components/Homepage';
+
 render(
   <Provider store={store}>
     <Router history={history}>
       <Route path="/" component={App}>
+        <IndexRoute component={Homepage}/>
       </Route>
     </Router>
   </Provider>,
