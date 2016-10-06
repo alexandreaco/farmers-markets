515afd82169e7c0d9e22cec6e5c08629843c6525
Alexandrea Defreitas <alexandreadef@gmail.com>
2016-10-06 14:43:50 -0400

    Adds ability to pass params object to callApi()

--- app/utils/api.js
+++ app/utils/api.js
@@ -5,10 +5,12 @@ import { API_URL } from '../config';
 
 // API Routes
 export const GET_ALL_FARMS = '/api';
+export const GET_FARMS_BY_STATE = '/api/farms';
 
 // API Caller
-export const callApi = (route) => {
-  const url = `${API_URL}${route}`;
+export const callApi = (route, params) => {
+  const paramsString = params ? Object.keys(params).map(key => `${key}=${params[key]}`).join('&') : '';
+  const url = `${API_URL}${route}?${paramsString}`;
   return fetch(url)
     .then(response => response.json())
     .then(json => {
